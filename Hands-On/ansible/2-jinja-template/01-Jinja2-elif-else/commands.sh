520174756523b5de95c8fcc5072f3f4079797162
atingupta2005 <atingupta2005@gmail.com>
2022-11-21 07:11:19 +0530

    Update commands.sh

--- Hands-On/ansible/2-jinja-template/01-Jinja2-elif-else/commands.sh
+++ Hands-On/ansible/2-jinja-template/01-Jinja2-elif-else/commands.sh
@@ -1,5 +1,7 @@
 source ~/python_venv/bin/activate
 
+cd ~/Bosch-Azure-Devops-Nov-22/Hands-On/ansible
+cd 2-jinja-template/01-Jinja2-elif-else
 ## Running Playbook
 ansible-playbook -i hosts facts_playbook.yml
 ansible-playbook -i hosts jinja2_playbook.yml
