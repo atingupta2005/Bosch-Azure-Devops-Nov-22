ef250fa0851f20d7ed9fa5c4847928fc1d41501d
atingupta2005 <atingupta2005@gmail.com>
2022-11-21 20:03:21 +0530

    Update README.md.sh

--- Hands-On/Kubernetes/03-resource-and-objects/04-02-PODs-with-YAML/README.md.sh
+++ Hands-On/Kubernetes/03-resource-and-objects/04-02-PODs-with-YAML/README.md.sh
@@ -11,9 +11,12 @@ kubectl apply -f kube-manifests/03-pod-LoadBalancer-service.yml
 # List Service
 kubectl get svc
 
-SVC_PUB_IP=$(kubectl get svc pod-LoadBalancer-service -o json | jq .status.loadBalancer.ingress[0].ip)
+SVC_PUB_IP=$(kubectl get svc myapp-pod-loadbalancer-service -o json | jq .status.loadBalancer.ingress[0].ip)
 SVC_PUB_IP2=${SVC_PUB_IP//\"}
 echo $SVC_PUB_IP2
 
 # Access Application
 curl $SVC_PUB_IP2
+
+kubectl delete -f kube-manifests/02-pod-definition.yml
+kubectl delete -f kube-manifests/03-pod-LoadBalancer-service.yml
