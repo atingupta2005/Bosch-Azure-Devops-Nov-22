82ceef47623d8edbffb01e6aa8d85e1f8449ac89
atingupta2005 <atingupta2005@gmail.com>
2022-11-21 21:03:37 +0530

    Update README.md.sh

--- Hands-On/Kubernetes/06-Persistent-and-ephemeral-volumes/05-02-UserMgmt-WebApp-with-MySQLDB/README.md.sh
+++ Hands-On/Kubernetes/06-Persistent-and-ephemeral-volumes/05-02-UserMgmt-WebApp-with-MySQLDB/README.md.sh
@@ -1,3 +1,5 @@
+cd ~/Bosch-Azure-Devops-Nov-22/Hands-On/Kubernetes/06-Persistent-and-ephemeral-volumes/05-02-UserMgmt-WebApp-with-MySQLDB/
+
 # Deploy UserManagement Web Application with MySQL Database
 
 # Create Deployment & LoadBalancer Service
