b1eeda2448ad3abadb0b00fba34e8b3a8408b589
atingupta2005 <atingupta2005@gmail.com>
2022-11-21 07:44:07 +0530

    Update commands.sh

--- Hands-On/ansible/3-dynamic-inventory/commands.sh
+++ Hands-On/ansible/3-dynamic-inventory/commands.sh
@@ -1,13 +1,16 @@
 source ~/python_venv/bin/activate
+cd ~/Bosch-Azure-Devops-Nov-22/Hands-On/ansible
+cd 3-dynamic-inventory
 
 az account list
 az account set --subscription '<subscription-name>'
+az account list
 
 ansible -u ansibleroot all -m ping -i inventory.azure_rm.yml
 
 ## Running Playbook
 #nano playbook.yml
-ansible-playbook -u ansibleroot playbook.yml -kK
+ansible-playbook -u ansibleroot playbook.yml -K
 
 ## Confirm content of the file in all hosts
 ansible -u ansibleroot all -a "cat /tmp/testfile.txt"
