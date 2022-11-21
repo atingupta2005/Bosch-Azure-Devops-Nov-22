6f130c8b4cd3cc8450627ae29accb2bd0ce660d1
atingupta2005 <atingupta2005@gmail.com>
2022-11-21 07:45:30 +0530

    Update commands.sh

--- Hands-On/ansible/2-jinja-template/03-Jinja2-template-file/commands.sh
+++ Hands-On/ansible/2-jinja-template/03-Jinja2-template-file/commands.sh
@@ -1,4 +1,6 @@
 source ~/python_venv/bin/activate
+cd ~/Bosch-Azure-Devops-Nov-22/Hands-On/ansible
+cd 2-jinja-template/03-Jinja2-template-file
 
 ## Running Playbook
 ansible-playbook -i hosts jinja2_playbook.yml
